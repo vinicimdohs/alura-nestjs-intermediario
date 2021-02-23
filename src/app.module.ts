15b930d98f2e2073bbcdd49282d74bc37390b332
Vinicius Fernandes de Paula <vinicim.fernandez1@gmail.com>
2021-02-23 10:54:23 -0300

    Tratando erros lidando tanto com fastfy quanto com express

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { FiltroDeExcecaoHttp } from './common/filter/filtro-de-excecao-htpp.filter';
 import { UsuarioModule } from './usuario/usuario.module';
 
 
@@ -10,6 +11,10 @@ import { UsuarioModule } from './usuario/usuario.module';
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor
+    },
+    {
+      provide: APP_FILTER,
+      useClass : FiltroDeExcecaoHttp
     }
   ],
 })
